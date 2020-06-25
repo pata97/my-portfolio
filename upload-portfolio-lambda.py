9216801758d5f280a40afb857661b0b07267867a
Pat Alexander <patalexander71.gmail.com>
2020-06-24 19:02:21 -0700

    Use bucket name and object key from codepipeline, and notify when codepipeline job is done.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,18 +3,33 @@ from botocore.client import Config
 import StringIO
 import zipfile
 
+
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:024223368277:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.patriciaalexander.net',
+        "objectKey": 'portfoliobuild.zip'
+    }
+
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"][inputArtifacts"]:
+                if artifact["name"] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+
+        print "Building portfolio from " + str(location)
+
         s3 = boto3.resource('s3', 'us-east-1', config=Config(s3={'addressing_style': 'path'}))
 
         portfolio_bucket = s3.Bucket('portfolio.patriciaalexander.net')
-        build_bucket = s3.Bucket('portfoliobuild.patriciaalexander.net')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -24,8 +39,12 @@ def lambda_handler(event, context):
 
         print "Job done!"
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+        if job
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully!")
         raise
 
-    return "Hello from Lambda"
+    return 'Hello from Lambda'
